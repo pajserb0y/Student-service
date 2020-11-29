5d1b50b01e62b8f13d75e78cfa76acb77f82e9ec
Pajserb0y <nikmarko555@gmail.com>
2020-11-29 04:32:11 +0100

    "#toolbar Dodaj nove ikonic i labele za search"

--- src/view/Toolbar.java
+++ src/view/Toolbar.java
@@ -1,11 +1,14 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.JToggleButton;
 import javax.swing.JToolBar;
 import javax.swing.SwingConstants;
 
@@ -22,34 +25,45 @@ public class Toolbar extends JToolBar {
 		
 		JButton btnNew = new JButton();
 		btnNew.setToolTipText("Open");
-		btnNew.setIcon(new ImageIcon("images/Screenshot 2020-11-25 194909.png"));
+		btnNew.setIcon(new ImageIcon("images/Screenshot 2020-11-25 194909(1).png"));
 		add(btnNew);
 	
 		addSeparator();
 	
 		JButton btnUredi = new JButton();
 		btnUredi.setToolTipText("Edit");
-		btnUredi.setIcon(new ImageIcon("images/Screenshot 2020-11-25 195241.png"));
+		btnUredi.setIcon(new ImageIcon("images/Screenshot 2020-11-25 195241(1).png"));
 		add(btnUredi);
 	
 		addSeparator();
 	
-		JToggleButton tglObrisi = new JToggleButton();
-		tglObrisi.setToolTipText("Delete");
-		tglObrisi.setIcon(new ImageIcon("images/Screenshot 2020-11-25 195314.png"));
-		add(tglObrisi);
+		JButton btnObrisi = new JButton();
+		btnObrisi.setToolTipText("Obriši");
+		btnObrisi.setIcon(new ImageIcon("images/Screenshot 2020-11-25 195314(1).png"));
+		add(btnObrisi);
 		
 		addSeparator();
 		
-		JTextField txtSearch = new JTextField();
-		txtSearch.setSize(10, 10);
-		add(txtSearch);
 		
+		JPanel panCenter=new JPanel();
+		BoxLayout boxCenter=new BoxLayout(panCenter, BoxLayout.X_AXIS);
+		panCenter.setLayout(boxCenter);
+
+		Dimension dim=new Dimension(180,25);
+		
+		JPanel panSearch=new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		JTextField txtSearch=new JTextField();
+        txtSearch.setPreferredSize(dim);
+		
+		panSearch.add(txtSearch);
+		panCenter.add(panSearch);
+        add(panCenter,BorderLayout.EAST);
+        
 		addSeparator();
 		
 		JButton btnSearch = new JButton();
 		btnSearch.setToolTipText("Search");
-		btnSearch.setIcon(new ImageIcon("images/Screenshot 2020-11-25 195357.png"));
+		btnSearch.setIcon(new ImageIcon("images/Screenshot 2020-11-25 195357(1).png"));
 		add(btnSearch, BorderLayout.EAST);
 	}
 }
