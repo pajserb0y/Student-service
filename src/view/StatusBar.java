18fb392e7b41ac44a2ca7ea96ffe9a605ce8cf4e
GGmaz <kajtes.nikola@gmail.com>
2020-12-25 21:27:15 +0100

    #dorada_mnemonika

--- src/view/StatusBar.java
+++ src/view/StatusBar.java
@@ -31,7 +31,7 @@ public class StatusBar extends JPanel{
 		left.setLayout(new FlowLayout(FlowLayout.LEFT,10,5));
 		
 		JLabel naziv = new JLabel("StatusBar");
-		naziv.setText("Studentska sluûba");
+		naziv.setText("Studentska slu≈æba");
 		left.add(naziv);
 		
 		JPanel right = new JPanel();
