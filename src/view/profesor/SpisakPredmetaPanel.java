feacde9ee9f7003fa8cee195a6e5964cbf3ea46b
GGmaz <kajtes.nikola@gmail.com>
2021-01-10 17:14:29 +0100

    #uklanjanje_predmeta_sa_profesora Omoguci uklanjanje predmeta sa profesora

--- src/view/profesor/SpisakPredmetaPanel.java
+++ src/view/profesor/SpisakPredmetaPanel.java
@@ -10,18 +10,24 @@ import java.util.ArrayList;
 
 import javax.swing.Box;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
+import controller.StudentController;
+import model.AbstractTableModelDozvoljeniPredmeti;
+import model.AbstractTableModelSpisakPredmetaZaProfesora;
 import model.BazaPredmeta;
 import model.BazaProfesora;
 import model.Predmet;
+import model.Student;
 import view.TabbedPane;
 
 public class SpisakPredmetaPanel extends JPanel{
 
 	public static JTable tabelaSpisakPredmeta = null;
+	
 	public SpisakPredmetaPanel() {
 		
 		// TODO Auto-generated constructor stub
@@ -48,7 +54,51 @@ public class SpisakPredmetaPanel extends JPanel{
 				dialog.setVisible(true);
 			}
 		});
+		
+		btnUkloniPredmet.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				//DozvoljeniPredmetiJTable TabelaDozvoljenihPredmeta = new DozvoljeniPredmetiJTable();
+				
+				int rowView = tabelaSpisakPredmeta.getSelectedRow();
+				int rowModel = tabelaSpisakPredmeta.convertRowIndexToModel(rowView);
+				if(rowModel != -1){
+					ArrayList<Predmet> spisakPredmetaProfesora = new ArrayList<Predmet>();
+					
+					if(BazaProfesora.getInstance().getRow(rowModel).getSpisakPredmeta() != null)
+						spisakPredmetaProfesora = BazaProfesora.getInstance().getRow(rowModel).getSpisakPredmeta();
+					
+					int izbor = JOptionPane.showConfirmDialog(null, "Da li ste sigurni?", "Ukloni predmet", JOptionPane.YES_NO_OPTION);
+					if (izbor == JOptionPane.YES_OPTION) {
+						
+						AbstractTableModelSpisakPredmetaZaProfesora mdp = (AbstractTableModelSpisakPredmetaZaProfesora) tabelaSpisakPredmeta.getModel();
+						String sifra = (String) mdp.getValueAt(rowModel, 0);
+						
+						int rowProfView = TabbedPane.tabelaProfesora.getSelectedRow();
+						int rowProfModel = TabbedPane.tabelaProfesora.convertRowIndexToModel(rowProfView); //profesor kom uklanjamo predmet
+						
 
+						for(Predmet p : BazaPredmeta.getInstance().getPredmeti())
+							if(p.getProfesor() != null)
+								if(sifra.equals(p.getSifraPred())) {
+									spisakPredmetaProfesora.remove(p);
+									p.setProfesor(null);
+								}
+								
+						BazaProfesora.getInstance().getRow(rowProfModel).setSpisakPredmeta(spisakPredmetaProfesora); //dodavanje profesoru predmeta
+						
+						AbstractTableModelSpisakPredmetaZaProfesora model =  (AbstractTableModelSpisakPredmetaZaProfesora) SpisakPredmetaPanel.tabelaSpisakPredmeta.getModel();
+						model.fireTableDataChanged();
+					}
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "Morate izabrati predmet pre izmene!");
+				}
+			}
+		});
+		
 		Box boxCentar = Box.createVerticalBox();
 		boxCentar.add(Box.createVerticalStrut(20));
 		
